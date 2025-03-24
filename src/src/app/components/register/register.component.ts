91cf05de38de14cb5708465cd2447762bb89429a
Smit Patel <smitkumar97@gmail.com>
2025-03-24 23:46:13 +0530

    merge commit

---
+++ src/src/app/components/register/register.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  registerForm = new FormGroup({
+    fullName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', Validators.required),
+  });
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      console.log('Form Submitted', this.registerForm.value);
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
+}
